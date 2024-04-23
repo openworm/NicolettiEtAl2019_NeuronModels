8e001f9ebe22b4a6c5686c87cdd016a4f86ea28b
pgleeson <p.gleeson@gmail.com>
2024-04-23 20:26:25 +0100

    Added shal egl32 unc2 channels

--- NeuroML2/GenerateNeuroML.py
+++ NeuroML2/GenerateNeuroML.py
@@ -3,18 +3,83 @@ from neuroml.utils import component_factory
 
 from pyneuroml import pynml
 from pyneuroml.xppaut import parse_script
+from pprint import pprint
 
 from neuroml import GateHHRates
 
 
 xpps = {"RMD": parse_script("../RMD.ode"), "AWCon": parse_script("../AWC.ode")}
-
-
+#xpps = {"RMD": parse_script("../RMD.ode")}
+#pprint(xpps)
 
 colors = {"AWCon": "0 0 0.8", "RMD": "0 0.8 0", "GenericMuscleCell": "0.8 0 0"}
 
+def _add_tc_ss(chan_id_in_cell, gate_id, inf_expr_param, tau_expr_param, chan_doc, xpp, extra_params):
+    
+    ss = component_factory("HHVariable", type="%s_%s_inf"%(chan_id_in_cell,gate_id)) 
+    tc = component_factory("HHTime", type="%s_%s_tau"%(chan_id_in_cell,gate_id)) 
 
-def create_channel_file(chan_id_in_cell, cell_id, xpp, species, gates={},extra_params=[]):
+    ssct = component_factory("ComponentType", name=ss.type, extends="baseVoltageDepVariable")
+    tcct = component_factory("ComponentType", name=tc.type, extends="baseVoltageDepTime")
+    chan_doc.add(ssct)
+    chan_doc.add(tcct)
+
+    vscale = component_factory("Constant", name='VOLT_SCALE',  dimension="voltage", value="1 mV")
+    ssct.add(vscale)
+    tcct.add(vscale)
+    tscale = component_factory("Constant", name='TIME_SCALE',  dimension="time", value="1 ms")
+    tcct.add(tscale)
+
+
+    inf_expr = xpp["derived_variables"][inf_expr_param]
+    tau_expr = xpp["derived_variables"][tau_expr_param]
+
+    potential_parameters = []
+    for p in xpp["parameters"]:
+        if chan_id_in_cell in p:
+            potential_parameters.append(p)
+
+    for p in potential_parameters+extra_params:
+        const = component_factory(
+            "Constant", name=p, dimension="none", value=str(xpp["parameters"][p])
+        )
+        if p in inf_expr:
+            ssct.add(const)
+        if p in tau_expr:
+            tcct.add(const)
+    
+
+    d = component_factory("Dynamics")
+    ssct.add(d)
+
+    dvv = component_factory("DerivedVariable", name="V", dimension="none", value="(v) / VOLT_SCALE")
+    d.add(dvv)
+    
+    import sympy
+    from sympy.parsing.sympy_parser import parse_expr
+    v,V = sympy.symbols('v V')
+
+    s_expr = parse_expr(inf_expr, evaluate=False)
+    s_expr = s_expr.subs(v,V)
+
+    dv = component_factory("DerivedVariable", name="x", exposure="x", dimension="none", value=s_expr)
+    d.add(dv)
+    
+    d = component_factory("Dynamics")
+    tcct.add(d)
+    d.add(dvv)
+
+    s_expr = parse_expr(tau_expr, evaluate=False)
+    s_expr = s_expr.subs(v,V)
+
+    dv = component_factory("DerivedVariable", name="t", exposure="t", dimension="time", value='(%s)/1000'%s_expr)
+    d.add(dv)
+
+    return ss, tc
+    
+
+def create_channel_file(chan_id_in_cell, cell_id, xpp, species, gates={}, extra_params=[]):
+
     chan_id = "%s_%s" % (cell_id, chan_id_in_cell)
     chan_doc = NeuroMLDocument(
         id=chan_id,
@@ -31,75 +96,32 @@ def create_channel_file(chan_id_in_cell, cell_id, xpp, species, gates={},extra_p
         notes="%s channel from Nicoletti et al. 2019" % chan_id,
     )
 
-    for g in gates:
+    for gate_id in gates:
 
-        ss = component_factory("HHVariable", type="%s_%s_inf"%(chan_id_in_cell,g)) 
-        tc = component_factory("HHTime", type="%s_%s_tau"%(chan_id_in_cell,g)) 
+        if type(gates[gate_id]) == list:
+            ss, tc = _add_tc_ss(chan_id_in_cell, gate_id, gates[gate_id][1], gates[gate_id][2], chan_doc, xpp, extra_params)
 
-        gc = component_factory("GateHHTauInf", id=g, instances=gates[g][0], steady_state=ss, time_course=tc, validate=True) 
-        channel.add(gc)
-        
-        ssct = component_factory("ComponentType", name=ss.type, extends="baseVoltageDepVariable")
-        tcct = component_factory("ComponentType", name=tc.type, extends="baseVoltageDepTime")
-        chan_doc.add(ssct)
-        chan_doc.add(tcct)
+            gc = component_factory("GateHHTauInf", id=gate_id, instances=gates[gate_id][0], steady_state=ss, time_course=tc, validate=True) 
+            channel.add(gc)        
 
-        vscale = component_factory("Constant", name='VOLT_SCALE',  dimension="voltage", value="1 mV")
-        ssct.add(vscale)
-        tcct.add(vscale)
-        tscale = component_factory("Constant", name='TIME_SCALE',  dimension="time", value="1 ms")
-        tcct.add(tscale)
+        elif type(gates[gate_id]) == dict:
 
+            gc = component_factory("GateFractional", id=gate_id, instances=1, validate=False) 
 
-        inf_expr = xpp["derived_variables"][gates[g][1]]
-        tau_expr = xpp["derived_variables"][gates[g][2]]
+            for sub_gate in gates[gate_id]:
+                # gates={'m':[3,'minf_shal','tm_shal'],'h':{'hf': [0.7,'hinf_shal','thf_shal'],'hs': [0.3,'hinf_shal','ths_shal']} } 
+                fract = gates[gate_id][sub_gate][0]
 
-        potential_parameters = []
-        for p in xpp["parameters"]:
-            if chan_id_in_cell in p:
-                potential_parameters.append(p)
+                ss, tc = _add_tc_ss(chan_id_in_cell, '%s_%s'%(gate_id,sub_gate), gates[gate_id][sub_gate][1], gates[gate_id][sub_gate][2], chan_doc, xpp, extra_params)
 
-        for p in potential_parameters+extra_params:
-            const = component_factory(
-                "Constant", name=p, dimension="none", value=str(xpp["parameters"][p])
-            )
-            if p in inf_expr:
-                ssct.add(const)
-            if p in tau_expr:
-                tcct.add(const)
+                sg = component_factory("GateFractionalSubgate", id=sub_gate, fractional_conductance=str(fract), steady_state=ss, time_course=tc, validate=True) 
+                gc.add(sg)
+            channel.add(gc)    
         
 
-        d = component_factory("Dynamics")
-        ssct.add(d)
-        dvv = component_factory("DerivedVariable", name="V", dimension="none", value="(v) / VOLT_SCALE")
-        d.add(dvv)
-        
-
-        import sympy
-        from sympy.parsing.sympy_parser import parse_expr
-        v,V = sympy.symbols('v V')
-
-        s_expr = parse_expr(inf_expr, evaluate=False)
-        s_expr = s_expr.subs(v,V)
-
-        dv = component_factory("DerivedVariable", name="x", exposure="x", dimension="none", value=s_expr)
-        d.add(dv)
-        
-        d = component_factory("Dynamics")
-        tcct.add(d)
-        d.add(dvv)
-
-        s_expr = parse_expr(tau_expr, evaluate=False)
-        s_expr = s_expr.subs(v,V)
-
-        dv = component_factory("DerivedVariable", name="t", exposure="t", dimension="time", value='(%s)/1000'%s_expr)
-        d.add(dv)
-        
             
-
-
     chan_doc.add(channel)
-    #chan_doc.validate(recursive=True)
+    chan_doc.validate(recursive=True)
 
     pynml.write_neuroml2_file(nml2_doc=chan_doc, nml2_file_name=chan_fn, validate=True)
 
@@ -163,7 +185,7 @@ def generate_nmllite(cell, duration=700, config="IClamp", parameters=None):
 
 
 def create_cells(channels_to_include):
-    for cell_id in ["AWCon", "RMD"]:
+    for cell_id in xpps.keys():
         # Create the nml file and add the ion channels
         cell_doc = NeuroMLDocument(
             id=cell_id, notes="A cell from Nicoletti et al. 2019"
@@ -196,6 +218,31 @@ def create_cells(channels_to_include):
                 ion_chan_def_file=create_channel_file("leak", cell_id, xpps[cell_id], species='non_specific'),
             )
 
+        # SHL-1 CHANNELS
+        if 'shal' in channels_to_include:
+            chan_id = 'shal'
+            ion = 'k'
+            g_param = 'gshal'
+            gates={'m':[3,'minf_shal','tm_shal'],'h':{'hf': [0.7,'hinf_shal','thf_shal'],'hs': [0.3,'hinf_shal','ths_shal']} } 
+            extra_params = []
+
+            cell.add_channel_density(
+                cell_doc,
+                cd_id="%s_chans"%chan_id,
+                cond_density="%s S_per_m2" % xpps[cell_id]["parameters"][g_param],
+                erev="%smV" % xpps[cell_id]["parameters"]["e%s"%ion],
+                ion=ion,
+                ion_channel="%s_%s" % (cell_id,chan_id),
+                ion_chan_def_file=create_channel_file(
+                    chan_id,
+                    cell_id,
+                    xpps[cell_id],
+                    species=ion,
+                    gates=gates,
+                    extra_params = extra_params
+                ),
+            )
+
         # SHK1 CHANNELS
         if 'shak' in channels_to_include:
             chan_id = 'shak'
@@ -203,6 +250,37 @@ def create_cells(channels_to_include):
             g_param = 'gshak'
             gates={'m':[1,'minf_shak','tm_shak'],'h':[1,'hinf_shak','th_shak']}
             extra_params = ['shiftV05']
+
+            cell.add_channel_density(
+                cell_doc,
+                cd_id="%s_chans"%chan_id,
+                cond_density="%s S_per_m2" % xpps[cell_id]["parameters"][g_param],
+                erev="%smV" % xpps[cell_id]["parameters"]["e%s"%ion],
+                ion=ion,
+                ion_channel="%s_%s" % (cell_id,chan_id),
+                ion_chan_def_file=create_channel_file(
+                    chan_id,
+                    cell_id,
+                    xpps[cell_id],
+                    species=ion,
+                    gates=gates,
+                    extra_params = extra_params
+                ),
+            )
+            
+        # EGL-36 CHANNELS
+        if cell_id is not "AWCon" and 'egl36' in channels_to_include:
+            chan_id = 'egl36'
+            ion = 'k'
+            g_param = 'gegl36'
+
+            gates={'m':{'m1': [xpps[cell_id]["parameters"]['a1'],'min1_egl36','tm1_egl36'],
+                        'm2': [xpps[cell_id]["parameters"]['a2'],'min2_egl36','tm2_egl36'],
+                        'm3': [xpps[cell_id]["parameters"]['a3'],'min3_egl36','tm3_egl36']} } 
+
+            extra_params = []
+
+            #print(xpps[cell_id]["parameters"].keys())
 
             cell.add_channel_density(
                 cell_doc,
@@ -236,6 +314,32 @@ def create_cells(channels_to_include):
                     xpps[cell_id],
                     species='k',
                     gates={'m':[1,'minf_kir','tm_kir']},
+                ),
+            )
+
+
+        # UNC-2 CHANNELS
+        if 'unc2' in channels_to_include:
+            chan_id = 'unc2'
+            ion = 'ca'
+            g_param = 'gunc2'
+            gates={'m':[1,'minf_unc2','tm_unc2'],'h':[1,'hinf_unc2','th_unc2']}
+            extra_params = ['stm2','sth2']
+
+            cell.add_channel_density(
+                cell_doc,
+                cd_id="%s_chans"%chan_id,
+                cond_density="%s S_per_m2" % xpps[cell_id]["parameters"][g_param],
+                erev="%smV" % xpps[cell_id]["parameters"]["e%s"%ion],
+                ion=ion,
+                ion_channel="%s_%s" % (cell_id,chan_id),
+                ion_chan_def_file=create_channel_file(
+                    chan_id,
+                    cell_id,
+                    xpps[cell_id],
+                    species=ion,
+                    gates=gates,
+                    extra_params = extra_params
                 ),
             )
 
@@ -294,4 +398,7 @@ if __name__ == "__main__":
     channels_to_include = ['leak']
     channels_to_include = ['leak','kir']
     channels_to_include = ['leak','kir','shak','cca']
+    channels_to_include = ['leak','shal','kir','shak','cca']
+    channels_to_include = ['leak','shal','egl36','kir','shak','cca']
+    channels_to_include = ['leak','shal','egl36','kir','shak','cca','unc2']
     create_cells(channels_to_include)
--- NeuroML2/analyseChans.sh
+++ NeuroML2/analyseChans.sh
@@ -1,4 +1,8 @@
-pynml-channelanalysis RMD_cca.channel.nml  -iv -clampDe 100 -clampDu 200 -dur 400 -erev 60 -maxV 40 -minV -80
-pynml-channelanalysis RMD_shak.channel.nml -iv -clampDe 100 -clampDu 700 -dur 800 -erev -80 -maxV 40 -minV -120  -clampBaseVoltage -80 -stepTargetVoltage 10
-pynml-channelanalysis RMD_kir.channel.nml -iv -clampDe 100 -clampDu 700 -dur 800 -erev -80 -maxV 40 -minV -120  -clampBaseVoltage -80 -stepTargetVoltage 10
 
+pynml-channelanalysis RMD_shak.channel.nml  -iv -clampDe 100 -clampDu 700 -dur 800 -erev -80 -maxV 40 -minV -120  -clampBaseVoltage -80 -stepTargetVoltage 10
+pynml-channelanalysis RMD_shal.channel.nml  -iv -clampDe 100 -clampDu 700 -dur 800 -erev -80 -maxV 40 -minV -120  -clampBaseVoltage -80 -stepTargetVoltage 10
+pynml-channelanalysis RMD_egl36.channel.nml -iv -clampDe 500 -clampDu 6000 -dur 7000 -erev -80 -maxV 40 -minV -120  -clampBaseVoltage -80 -stepTargetVoltage 10
+pynml-channelanalysis RMD_kir.channel.nml   -iv -clampDe 100 -clampDu 700 -dur 800 -erev -80 -maxV 40 -minV -120  -clampBaseVoltage -80 -stepTargetVoltage 10
+
+pynml-channelanalysis RMD_cca.channel.nml   -iv -clampDe 100 -clampDu 200 -dur 400 -erev 60 -maxV 40 -minV -80  -clampBaseVoltage -80 -stepTargetVoltage 10
+pynml-channelanalysis RMD_unc2.channel.nml  -iv -clampDe 100 -clampDu 200 -dur 400 -erev 60 -maxV 40 -minV -80  -clampBaseVoltage -80 -stepTargetVoltage 10
